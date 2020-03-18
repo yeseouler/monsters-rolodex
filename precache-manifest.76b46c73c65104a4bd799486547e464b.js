c121c8ae291887375b55925719436d2c2b3b4624
Yeseul Jeon <yeseouler52@gmail.com>
2020-03-18 23:23:40 +0900

    Updates

---
+++ precache-manifest.76b46c73c65104a4bd799486547e464b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ff4b7028aeedb22139bc4c9f9b5d6a0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "07db82df78edd830b3a8",
+    "url": "/monsters-rolodex/static/css/main.228b2bcc.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07db82df78edd830b3a8",
+    "url": "/monsters-rolodex/static/js/main.1d97ba2c.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
